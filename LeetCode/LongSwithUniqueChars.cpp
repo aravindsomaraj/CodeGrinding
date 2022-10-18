7e48698542bc616fd4532fef185ebee1c3a88b03
Aravind Somaraj <buu@DESKTOP-ICGDGU6.localdomain>
2022-10-19 00:47:42 +0530

    a

---
+++ LeetCode/LongSwithUniqueChars.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        vector<int> mp (256,0);
+        
+        // if(s=="")
+        //     return 0;
+        
+        int start=0,maxl=0,maxs=0,l=0;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]]++;
+            cout << s[i];
+            
+            if(mp[s[i]]!=1)
+            {
+                while(mp[s[i]]!=1)
+                {
+                    mp[s[start]]--;
+                    start++;
+                }
+            }
+            l=i-start+1;
+            if(l>maxl)
+            {
+                maxl=l;
+                maxs=start;
+            }
+        }
+        
+        return maxl;
+        
+    }
+    
+};
